edd2357ed268b17ebea9502c61d514ead1eef383
SteevenSF01 <salgadosteeven@gmail.com>
2024-05-06 10:25:55 +0200

    fin de toute la section hero

--- Portfolio/hero_app/views.py
+++ Portfolio/hero_app/views.py
@@ -5,8 +5,9 @@ from hero_app.forms import HeroForm
 # Create your views here.
 def modify_hero(request, id):
     hero = HeroModel.objects.get(id=id)
+    heroAll = HeroModel.objects.all()
     form = HeroForm(request.POST or None, instance=hero)
     if form.is_valid():
         form.save()
-        return redirect('')
-    return render(request, 'modify_hero.html', {'form':form})
+        return redirect('/settings/modify_hero/1')
+    return render(request, 'modify_hero.html', {'form':form, 'heroall':heroAll})
